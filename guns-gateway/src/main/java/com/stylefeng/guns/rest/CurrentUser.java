30fffd3802a55c36f1bdc274b28ea5e917642b5c
monpro <monproshen@gmail.com>
2020-06-16 20:36:30 +1000

    save userId in threadLocal with jwt

---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/CurrentUser.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest;
+
+import com.style.guns.api.user.UserInfoModel;
+
+public class CurrentUser {
+
+    private static final ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    public static void saveUserId(String userId) {
+        threadLocal.set(userId);
+    }
+
+    public static String getCurrentUser() {
+        return threadLocal.get();
+    }
+}
