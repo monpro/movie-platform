e6fe90d108fe0c12452a09cce5cc50004ef794c2
monpro <monproshen@gmail.com>
2020-11-21 15:02:37 +1100

    add basic film classes

---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/vo/FilmIndexVO.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.modular.film.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class FilmIndexVO {
+
+    private List<BannerVO> bannerVOList;
+    private FilmVO hotFilms;
+    private FilmVO soonFilms;
+    private List<FilmInfo> boxRanking;
+    private List<FilmInfo> expectRanking;
+    private List<FilmInfo> top100;
+}
---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/vo/FilmInfo.java
@@ -0,0 +1,18 @@
+package com.stylefeng.guns.rest.modular.film.vo;
+
+import lombok.Data;
+
+@Data
+public class FilmInfo {
+
+    private String filmId;
+    private int filmType;
+    private String imgAddress;
+    private String filmName;
+    private String filmScore;
+    private int expectNum;
+    private String showTime;
+    private int boxNum;
+    // for top 100
+    private String score;
+}
