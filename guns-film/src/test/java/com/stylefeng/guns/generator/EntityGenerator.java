53411af978dc547535decd9215b9f6c4cc0246c9
monpro <monproshen@gmail.com>
2020-11-26 14:10:54 +1100

    finish film detail impl

--- guns-film/src/test/java/com/stylefeng/guns/generator/EntityGenerator.java
+++ guns-film/src/test/java/com/stylefeng/guns/generator/EntityGenerator.java
@@ -56,7 +56,7 @@ public class EntityGenerator {
         //strategy.setTablePrefix(new String[]{"_"});// 此处可以修改为您的表前缀
         strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
 //        strategy.setInclude(new String[]{"mooc_banner_t", "mooc_cat_dict_t", "mooc_area_dict_t", "mooc_year_dict_t", "mooc_film_t", "mooc_film_info_t", "mooc_actor_t"});
-        strategy.setInclude("mooc_source_dict_t");
+        strategy.setInclude("mooc_film_actor_t");
 
         mpg.setStrategy(strategy);
 
