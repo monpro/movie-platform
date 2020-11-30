e6d551bd564166cf958fa4f872798d46db15f036
monpro <monproshen@gmail.com>
2020-11-30 19:13:12 +1100

    generate mappers for cinema module

---
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocFieldTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocFieldT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 放映场次表 Mapper 接口
+ * </p>
+ *
+ * @author monpro
+ * @since 2020-11-30
+ */
+public interface MoocFieldTMapper extends BaseMapper<MoocFieldT> {
+
+}
