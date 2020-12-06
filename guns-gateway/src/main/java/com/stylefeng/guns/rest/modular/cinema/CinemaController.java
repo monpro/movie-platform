deefe6501d9e91fe26322020465bcfc9bb2794d6
monpro <monproshen@gmail.com>
2020-12-06 16:39:53 +1100

    add getCinemas controller

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/cinema/CinemaController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/cinema/CinemaController.java
@@ -2,13 +2,17 @@ package com.stylefeng.guns.rest.modular.cinema;
 
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.style.guns.api.cinema.CinemaServiceAPI;
 import com.style.guns.api.cinema.vo.CinemaQueryVO;
+import com.style.guns.api.cinema.vo.CinemaVO;
 import com.stylefeng.guns.rest.modular.vo.ResponseVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/cinema/")
 public class CinemaController {
@@ -19,7 +23,22 @@ public class CinemaController {
     @RequestMapping(value = "getCinemas")
     public ResponseVo getCinemas(CinemaQueryVO cinemaQueryVO) {
 
-        return null;
+        try {
+            Page<CinemaVO> cinemas = cinemaServiceAPI.getCinemas(cinemaQueryVO);
+            if (cinemas.getRecords() == null || cinemas.getRecords().size() == 0) {
+                return ResponseVo.serviceSuccess("no cinemas record");
+            } else {
+                return ResponseVo.serviceSuccess(
+                        cinemas.getCurrent(),
+                        (int) cinemas.getPages(),
+                        "",
+                        cinemas.getRecords()
+                );
+            }
+        } catch (Exception e) {
+            log.error("exception at cinema controller", e);
+            return ResponseVo.serviceFail("get cinemas failed");
+        }
     }
 
     @RequestMapping(value = "getCondition")
